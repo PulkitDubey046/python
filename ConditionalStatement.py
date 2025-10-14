83326bfaf5712fe50ca4cee12d9f2a396b6dad39
Pulkit Dubey <46dubey@gmail.com>
2025-10-14 10:08:02 +0530

    coding

--- ConditionalStatement.py
+++ ConditionalStatement.py
@@ -1,1 +1,9 @@
 # check a number is greater than or less than 99
+number = int(input("Enter a number: "))
+
+if number > 99:
+    print(f"{number} is greater than 99.")
+elif number < 99:
+    print(f"{number} is less than 99.")
+else:
+    print(f"{number} is equal to 99.")
