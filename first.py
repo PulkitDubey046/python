9cce2c0b838947c737ddba270c201a488aae7a16
Pulkit Dubey <46dubey@gmail.com>
2025-10-14 04:26:58 +0000

    first program

---
+++ first.py
@@ -0,0 +1,29 @@
+# My first program
+print("Hello World")
+#o/p Hello World
+
+# Datatypes in Python
+
+num = 1
+decimal = 1.3
+string = 'mike'
+boolean = True      # if you write true is will not work
+print(type(num))
+print(type(decimal))
+print(type(string))
+print(type(boolean))
+# output:-
+    # <class 'int'>
+    # <class 'float'>
+    # <class 'str'>
+    # <class 'bool'>
+
+# AIRTHMATIC OPERATIONS
+
+a,b=5,8
+print("Addition=", a+b)
+print("Subraction=", a-b)
+print("Multiplication=", a*b)
+print("Division=", a/b)
+print("Floor Division=", a//b)
+
