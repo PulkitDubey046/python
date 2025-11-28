4cecba835252a896967a5ccfc12453715be046bf
Pulkit Dubey <46dubey@gmail.com>
2025-11-28 18:53:31 +0530

    Adding espace sequences

---
+++ coding/slicing.py
@@ -0,0 +1,12 @@
+s1 = "Hello World"
+"""
+Syntax of indexing: string[index]
+Syntax of slicing: string[start:end:step]
+    -start: starting index at which the slicing operatin starts
+    -end: ending index at which the slicing stops (excluded)
+    -step: integer that specifies the step for the slicing 
+"""
+
+print(s1[2:7:1])
+print(s1[2:10:2])
+print(s1[2:12:3]) # in indexing we can
